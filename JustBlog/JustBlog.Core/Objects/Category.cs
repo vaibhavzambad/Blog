3c447f99f1bff0a043f9cb82bd66ce5fdc829530
vaibhavzambad <vaibhav.zambad@metacube.com>
2018-12-06 02:05:04 +0530

    Initial Commit For Blog

---
+++ JustBlog/JustBlog.Core/Objects/Category.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustBlog.Core.Objects
+{
+    public class Category
+    {
+        public virtual int Id
+        {
+            get;
+            set;
+        }
+
+        public virtual string Name
+        {
+            get;
+            set;
+        }
+
+        public virtual String Description
+        {
+            get;
+            set;
+        }
+
+        public virtual IList<Post> Posts
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ JustBlog/JustBlog.Core/Objects/Post.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JustBlog.Core.Objects
+{
+    public class Post
+    {
+        public virtual int Id
+        {
+            get;
+            set;
+        }
+
+        public virtual string Title
+        {
+            get;
+            set;
+        }
+
+        public virtual string ShortDescription
+        {
+            get;
+            set;
+        }
+
+        public virtual string Description
+        {
+            get;
+            set;
+        }
+
+        public virtual string Meta
+        {
+            get;
+            set;
+        }
+
+        public virtual string UrlSlug
+        {
+            get;
+            set;
+        }
+
+        public virtual bool IsPublished
+        {
+            get;
+            set;
+        }
+
+        public virtual DateTime PostedOn
+        {
+            get;
+            set;
+        }
+
+        public virtual DateTime? ModifiedOn
+        {
+            get;
+            set;
+        }
+
+        public virtual Category Category
+        {
+            get;
+            set;
+        }
+
+        public virtual IList<Tag> Tags
+        {
+            get;
+            set;
+        }
+    }
+}
